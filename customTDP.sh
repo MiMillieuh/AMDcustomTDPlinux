428fd149d6415b88fbdc7f1d2cebc6f9ad954ffc
MiMillieuh <52078885+MiMillieuh@users.noreply.github.com>
2022-10-02 23:53:20 +0200

    Add files via upload

--- customTDP.sh
+++ customTDP.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./ryzenadj --stapm-limit=9000 --fast-limit=25000 --slow-limit=9000 --tctl-temp=>
+./ryzenadj --stapm-limit=9000 --fast-limit=25000 --slow-limit=9000 --tctl-temp=90
 
 sleep 30 
 
