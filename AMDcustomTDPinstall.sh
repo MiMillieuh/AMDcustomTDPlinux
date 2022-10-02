7b65ff8e1f93cc3a46dd7380415e6080d5f3755c
MiMillieuh <52078885+MiMillieuh@users.noreply.github.com>
2022-10-02 23:51:57 +0200

    Add files via upload

---
+++ AMDcustomTDPinstall.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+if [[ "$USER" != "root" ]]; then
+    echo "Error: script not running as root or with sudo! Exiting..."
+    exit 1
+fi
+
+git clone https://github.com/FlyGoat/RyzenAdj.git
+
+cd RyzenAdj
+
+mkdir build && cd build
+
+cmake -DCMAKE_BUILD_TYPE=Release ..
+
+make
+
+cd ../
+
+mv -r build /opt/ryzenadj
+
+cd /opt/ryzenadj
+
+wget https://raw.githubusercontent.com/MiMillieuh/AMDcustomTDPlinux/main/customTDP.sh
+
+chmod +x customTDP.sh
+
+wget https://raw.githubusercontent.com/MiMillieuh/AMDcustomTDPlinux/main/amd-custom-tdp.service
+
+mv amd-custom-tdp.service /etc/systemd/system/
+
+systemctl enable amd-custom-tdp.service
+systemctl start amd-custom-tdp.service
+
+echo "now installed"
+
+exit 0
+
