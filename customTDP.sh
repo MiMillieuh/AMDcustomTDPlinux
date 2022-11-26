93a9ae350458e92f0b1ffc70b3685742e3db3a96
MiMillieuh <52078885+MiMillieuh@users.noreply.github.com>
2022-11-26 17:40:37 +0100

    Update customTDP.sh

--- customTDP.sh
+++ customTDP.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-/opt/ryzenadj/ryzenadj --stapm-limit=9000 --fast-limit=25000 --slow-limit=9000 >
+/opt/ryzenadj/ryzenadj --stapm-limit=25000 --fast-limit=25000 --slow-limit=25000 >
 
 sleep 30 
 
